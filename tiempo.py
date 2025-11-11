1f829744117a1214cc427ef7a116b7f0e6302a50
juances2001 <juances2001@gmail.com>
2025-11-11 09:22:32 -0500

    nuevo comentario

--- tiempo.py
+++ tiempo.py
@@ -1,7 +1,9 @@
 # Realice un software que determine cuantos minutos hay en x horas.
 
-minutos = float(input("Digite cantidad de horas: "))
+horas = float(input("Digite cantidad de horas: "))
 
-total_minutos = minutos * 60
+total_minutos = horas * 60
 
 print(f"Resultado total de minutos {total_minutos}")
+
+# fin del codigo
